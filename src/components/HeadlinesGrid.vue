54927cb6bc53132d9f8e1eb0d3189f0f7f22fa63
Patrik Pindes <ppindes@gmail.com>
2020-05-14 16:45:58 +0200

    fetching static headlines

--- src/components/HeadlinesGrid.vue
+++ src/components/HeadlinesGrid.vue
@@ -1,19 +1,41 @@
 <template>
  <v-container fluid>
     <v-row justify="center">
+      <v-container v-if="isLoading" class="mx-auto">
+        <span>Loadings news...</span>
+      </v-container>
       <HeadlineCard v-for="n in 10" :key="n"></HeadlineCard>
     </v-row>
  </v-container>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
 import HeadlineCard from './HeadlineCard.vue';
 
 export default {
+  data() {
+    return {
+      isLoading: true,
+    };
+  },
   components: {
     HeadlineCard,
   },
-
+  methods: {
+    ...mapActions([
+      'fetchAllHeadlines',
+    ]),
+  },
+  created() {
+    console.log('HeadlinesGridComponent created hook');
+    this.fetchAllHeadlines()
+      .then(/* Handled by store */)
+      .catch(() => {
+        console.log('Failed getting headlines. Try to reload the page.');
+      }) // show in modal or somethign, maybe provide button for reload
+      .finally(() => { this.isLoading = false; });
+  },
 };
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,28 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import fetchHeadlines from '../api/dummy-headlines';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    headlines: [],
+  },
+  getters: {
+    getHeadlines(state) {
+      return state.headlines;
+    },
   },
   mutations: {
+    setHeadlines(state, headlines) {
+      state.headlines = headlines;
+    },
   },
   actions: {
+    fetchAllHeadlines({ commit }) {
+      console.log('fetching headlines action activated');
+      return fetchHeadlines().then((headlines) => commit('setHeadlines', headlines));
+    },
   },
   modules: {
   },
